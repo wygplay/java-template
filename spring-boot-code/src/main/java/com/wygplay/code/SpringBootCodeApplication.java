aef64b131a262e0f034d189199086924d3b1bd06
wangyg <wangyg8@asiainfo.com>
2021-08-11 20:26:19 +0800

    java代码模板工程初始化

---
+++ spring-boot-code/src/main/java/com/wygplay/code/SpringBootCodeApplication.java
@@ -0,0 +1,16 @@
+package com.wygplay.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author WYG
+ */
+@SpringBootApplication
+public class SpringBootCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCodeApplication.class, args);
+    }
+
+}
